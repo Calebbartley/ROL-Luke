1e625adb7e4c78db3b3a8a50cf7db40b601ba96a
cbartley <cbartley@tdstickets.com>
2023-09-17 15:27:23 -0400

    navigation and routing added

--- rol-lukenashon/src/App.js
+++ rol-lukenashon/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import AboutUs from './components/about/AboutUs';
+import Navbar from './components/navigation/Navbar';
+import ContactUs from './components/contact/ContactUs';
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/">{Home}</Route>
+        <Route path="/about">{AboutUs}</Route>
+        <Route path="/contact">{ContactUs}</Route>
+      </Routes>
+    </Router>
     </div>
   );
 }
--- rol-lukenashon/src/components/home/Home.js
+++ rol-lukenashon/src/components/home/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import AboutUs from "../about/AboutUs";
 import ContactUs from "../contact/ContactUs";
 import Login from "../login/Login";
+import "./Home.css";
 
 function Home() {
   return (
-    <div className="home">
-      <div className="title">RICH OFF LOCKS</div>
-      <div className="logo"> LOGO IDEA </div>
-      <div className="navigationBar">
+    <div className="Home">
+      <div className="Title">RICH OFF LOCKS</div>
+      <div className="Logo"> LOGO IDEA </div>
+      <div className="NavigationBar">
         <AboutUs />
         <Login />
         <ContactUs />
